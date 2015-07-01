cc0c3f140a47195f75f7e3151726c21ef316ba63
Hello <me@you.com>
2015-07-02 04:15:48 +1000

    Added primitive strawpoll command.

---
+++ commands/strawpoll.js
@@ -0,0 +1,55 @@
+/**
+ * Creates a simple strawpoll.
+ *
+ * !strawpoll [title],[item1],[item2],[item3],[item4]
+ */
+
+var args = process.argv.splice(2);
+var request = require('request');
+var locallydb = require('locallydb');
+var db = new locallydb('db/_app');
+var commandsDb = db.collection('custom_commands');
+var moderatorDb = db.collection('channel_moderators');
+
+var user = JSON.parse(args[1]);
+
+var strawpoll_api = 'https://strawpoll.me/api/v2/polls';
+
+var poll_args = (((args[2].split(' ')).splice(1)).join(' ')).split(','); //im so sorry.
+var poll_title = poll_args[0];
+var poll_answers = poll_args.splice(1);
+
+// only show if the broadcaster
+if( isMod(args[0], user.username) ){
+	var strawpoll_id = 0;
+
+	request.post(
+		{
+			url: strawpoll_api,
+			form: {
+				options: poll_answers,
+				title: poll_title
+			}
+		},
+		function(err, response, body){
+			if( err === null ){
+				strawpoll_id = JSON.parse(body).id;
+
+				process.send({
+					'command': 'say',
+					'channel': args[0],
+					'message': 'Strawpoll here: http://strawpoll.me/' + strawpoll_id
+				});
+			}
+		}
+	);
+}
+
+function isMod(channel, username){
+	var channelMods = moderatorDb.where({
+		'channel': channel,
+		'username': username
+	});
+
+	return (channelMods.items[0].username === username);
+}
