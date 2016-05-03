ff2726f291d7760dc940c1f1d9089c922babed15
Andy S <a.simeon@gmail.com>
2016-05-04 00:36:41 +1000

    initial linedcmds

---
+++ modules/linedcmds.js
@@ -0,0 +1,60 @@
+"use strict";
+
+var _config = require("../config/config.user.js");
+var _client = undefined;
+var consts = require("../consts.js");
+var db = require("../mysqlHelpers.js");
+var util = require("../util.js");
+
+// only count lines per channel
+var loki = require("lokijs");
+var ldb = new loki("linedcmdsDb");
+var coll = ldb.addCollection("linedcmds");
+
+var tbl = "linedcmds";
+
+var onChat = (channel, user, message, self) => {
+	if(!self){
+		var chan = coll.find({"Channel": channel});
+
+		if(chan.length === 0){
+			coll.insert({
+				"Channel": channel,
+				"Lines": 0
+			});
+		}
+		else {
+			chan[0].Lines++;
+
+			coll.update(chan);
+		}
+
+		db.find(db.db(), tbl, {"Channel": channel}, (rows) => {
+			if(rows.length === 1){
+				var lines = rows[0].Lines;
+
+				if(chan.length && chan[0].Lines >= lines){
+					var cmds = JSON.parse(rows[0].Commands);
+					var rnd = Math.floor(Math.random() * cmds.length);
+
+					db.find(db.db(), "customcommand", {"Channel": channel, "Command": cmds[rnd]}, (rows) => {
+						if(rows.length === 1){
+							_client.say(channel, rows[0].OutputText);
+							chan[0].Lines = 0;
+							coll.update(chan);
+						}
+					});
+				}
+			}
+		});
+	}
+};
+
+var linedcmds = {
+	"register": (client) => {
+		_client = client;
+	},
+	"onChat": onChat
+};
+
+module.exports = linedcmds;
