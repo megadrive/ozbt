af9dc8ce0df774d22e2bb7b6dd666ba852113957
Andy S <a.simeon@gmail.com>
2017-04-05 22:49:51 +1000

    seriously, why did i make it the other way

--- core/search.js
+++ core/search.js
@@ -2,13 +2,18 @@
 
 var util = require("../util.js");
 var db = require("../dbHelpers.js");
-var gapi = require("../config/googleapi.js");
-var google = require('googleapis');
-var googleAuth = require('google-auth-library');
-var user = JSON.parse(process.env.user);
-var fs = require("fs");
-var readline = require("readline");
 var fuzzy = require("fuzzy");
+var consts = require("../consts.js");
+
+/**
+ * This block makes testing this individual function easier.
+ */
+util.say = process.env.channel ? util.say : util.say = (c, m) => { console.info(c, m) };
+process.env.user = process.env.user ? process.env.user : '{"username": "dummy", "mod": true}';
+process.env.channel = process.env.channel ? process.env.channel : "#megadriving";
+process.env.message = process.env.message ? process.env.message : "!search spyro";
+
+var user = JSON.parse(process.env.user);
 
 // Get arguments.
 let args = process.env.message.split(" ");
@@ -17,112 +22,30 @@ let query = args.splice(1).join(" ");
 let ssheetid = "1GHSGtC2NkudIH3a4UFreE2BImo2K2FBBM6GyQvZmM1E";
 let range = ["A13", "B1714"];
 
-authorize(gapi)
-	.then(function(token){
-		getGameNames(token)
-			.then(function(games){
-				console.info("Number of games:", games.length);
-				var results = fuzzy.filter(query, games, {"extract": function(el){ return el[0]; } });
-				var matches = results.map(function(el){
-					return el.string;
-				});
-
-				let top3 = matches.slice(0, 3);
-				let say = `Best ${top3.length} matches: ` + top3.join(", ") + ". ";
-				if(matches.length > 3) say += matches.length + " total matches.";
-				util.say(process.env.channel, util.getDisplayName(user) + ' -> ' + say);
-			});
-	});
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- */
-function authorize(credentials) {
-	return new Promise(function(resolve, reject){
-		var clientSecret = credentials.installed.client_secret;
-		var clientId = credentials.installed.client_id;
-		var redirectUrl = credentials.installed.redirect_uris[0];
-		var auth = new googleAuth();
-		var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-		db.find("api_credentials", {"Type": "GoogleSheets"})
-			.then(function(curr){
-				if(curr === null){
-					// No current API token
-					getNewToken(oauth2Client)
-						.then(function(token){
-							console.info("token:", token);
-						});
-				}
-				else {
-					// Active token, use this.
-					oauth2Client.credentials = curr;
-					resolve(oauth2Client);
-				}
-			});
-	});
+if(!util.checkPermissionCore(process.env.channel, user, consts.access.moderator)){
+	console.info("!search failed, not mod:", user.username);
+	process.exit(0);
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- */
-function getNewToken(oauth2Client) {
-	return new Promise(function(resolve, reject){
-		var authUrl = oauth2Client.generateAuthUrl({
-			access_type: 'offline',
-			scope: SCOPES
-		});
-		console.log('Authorize this app by visiting this url: ', authUrl);
-		var rl = readline.createInterface({
-			input: process.stdin,
-			output: process.stdout
-		});
-		rl.question('Enter the code from that page here: ', function(code) {
-			rl.close();
-			oauth2Client.getToken(code, function(err, token) {
-			if (err) {
-				console.log('Error while trying to retrieve access token', err);
-				return;
-			}
-			oauth2Client.credentials = token;
-			token["Type"] = "GoogleSheets";
-			db.insert("api_credentials", token)
-				.then(function(){
-					resolve(oauth2Client);
-				});
-			});
-		});
-	});
-}
+var request = require("request-promise");
+let api_key = "AIzaSyBHIbQxJidKiQwYt2QWu34ZyFJwHX49UCc";
+request(`https://sheets.googleapis.com/v4/spreadsheets/${ssheetid}/values/Games%20list!${range.join(":")}?key=${api_key}`)
+	.then(function(games_data){
+		let games = JSON.parse(games_data).values;
+		console.info("Number of games:", games.length);
 
-/**
- * Print the names and majors of students in a sample spreadsheet:
- * https://docs.google.com/spreadsheets/d/1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms/edit
- */
-function getGameNames(auth) {
-	return new Promise(function(resolve, reject){
-		var sheets = google.sheets('v4');
-		sheets.spreadsheets.values.get({
-			auth: auth,
-			spreadsheetId: ssheetid,
-			range: 'Games list!' + range.join(":"),
-		}, function(err, response) {
-			if (err) {
-				console.log('The API returned an error: ' + err);
-				return;
-			}
-			var rows = response.values;
-			if (rows.length == 0) {
-				reject(new Error('No data found.'));
-			} else {
-				resolve(rows);
-			}
-		});
+		if(query.length){
+			var results = fuzzy.filter(query, games, {"extract": function(el){ return el[0]; } });
+			var matches = results.map(function(el){
+				return el.string;
+			});
+
+			let top3 = matches.slice(0, 3);
+			let say = `Best ${top3.length} matches: ` + top3.join(", ") + ". ";
+			if(matches.length > 3) say += matches.length + " total matches.";
+			util.say(process.env.channel, util.getDisplayName(user) + ' -> ' + say);
+		}
+		else {
+			util.say(process.env.channel, util.getDisplayName(user) + ' -> Total games in PS1 challenge: ' + games.length);
+		}
 	});
-}
